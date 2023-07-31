ac826d3a592b8cadfb02a8f34e77fb29c19a0a0b
kli210 <kellyli2000.kl@gmail.com>
2023-07-30 20:45:14 -0400

    added download stopwords

--- app_reciperecs.py
+++ app_reciperecs.py
@@ -8,6 +8,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 import gensim.models.keyedvectors as word2vec
 import string
 import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 
 # Create a function for tokenizer
